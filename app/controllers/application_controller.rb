0891607d40317fefb4090280c88369c23f496a93
Jonathan East <jonathan_east@ymail.com>
2020-08-04 17:36:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    @user = User.new(:username => params[:username], :password => params[:passwprd])
-    if @user.save
-      redirect "/login"
-    else
-      redirect "/failure"
-    end
+    if params[:username] == "" || params[:password] == ""
   end
 
   get '/account' do
